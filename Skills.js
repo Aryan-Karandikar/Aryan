972cd4b4aaa02f33ffbe9a0210b48843c474a49a
Aryan-Karandikar <samuncle200@gmail.com>
2024-11-14 15:11:21 +0530

    Add files via upload

---
+++ Skills.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <section id="skills" className="section-container">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>HTML/CSS</li>
+        <li> MYSQL </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
---
+++ projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects" className="section-container">
+      <h2>Projects</h2>
+      <ul>
+        <li>Music Library System</li>
+        <li>Smart HealthCare Project</li>
+      
+       
+      </ul>
+    </section>
+  );
+}
+
+export default Projects;
